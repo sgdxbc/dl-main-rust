e6963805fd617476248d6326cac1c83570ab91d9
sgdxbc <sgdxbc@gmail.com>
2021-08-05 21:03:45 -0700

    start: Compile C example both as executable and library
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- start.sh
+++ start.sh
@@ -6,11 +6,13 @@ mkdir -p build
 
 cc -o build/run runtime.c -ldl
 
+cc -o build/c_exe c_example.c
 cc -o build/libc_example.so c_example.c -fPIC -shared
 
 pushd dloadable
 cargo build
 popd
 
+./build/c_exe hello world 42
 ./build/run ./build/libc_example.so
 ./build/run ./dloadable/target/debug/libdloadable.so
